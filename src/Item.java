a366a4373999d1b4f1b80e39f2a1892127593704
Joshua DCunha <joshdcunha99@gmail.com>
2020-04-17 20:26:45 +0530

    Made first commit

---
+++ src/Item.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+
+class Item {
+
+    String name;
+    int cost;
+    Map<String, Part> parts;
+
+    Item(String name, int cost, Map<String, Part> parts) {
+        this.name = name;
+        this.cost = cost;
+        this.parts = parts;
+    }
+}
